a02da1366c6d8d4012dd943a869df76b19142c39
Jacob Bender <benderjacob44@gmail.com>
2014-09-21 17:39:16 -0400

    Added support for finding all factors. Also fixed a bug. Refactored for loop and variables.

--- main.cpp
+++ main.cpp
@@ -3,23 +3,26 @@
 using namespace std;
 
 int factor_this;
-int divided_by, up_to;
+int divided_by, how_many;
 char proceed;
 
 int main()
 {
-    cout << "How many factors do you want listed?" << endl;
-    cin >> up_to;
-    up_to = up_to + 1;
-    cout << "Next, please enter a number to see it's factors." << endl;
+    cout << "Please enter a number to see it's factors." << endl;
     cin >> factor_this;
-    for (divided_by=1; (divided_by < up_to) && (divided_by < factor_this);)
+
+    cout << "How many factors do you want listed?" << endl;
+    cin >> how_many;
+    if (how_many == -1){
+	how_many == factor_this;
+	}
+    for (divided_by=1;(how_many!=0)&&(divided_by < factor_this);++divided_by)
         {
             if (factor_this%divided_by == 0)
             {
                 cout << factor_this << " divided by " << divided_by << " is " << (factor_this/divided_by) << endl;
-            }
-             ++divided_by;
+            	how_many-=1;
+	    }
         }
     cout << "Would you like to calculate more? Y for yes, N for no." << endl;
     cin >> proceed;
