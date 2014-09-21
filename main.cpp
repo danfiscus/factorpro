19dd309c4c782cad71410460b0103f9235c3b7c8
Dan Fiscus <danfiscus@users.noreply.github.com>
2014-09-21 11:23:06 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,20 +10,20 @@ int main()
 {
 
     begin:
-    cout << "How many factors do you want listed?" << '\n';
+    cout << "How many factors do you want listed?" << endl;
     cin >> up_to;
     up_to = up_to + 1;
-    cout << "Next, please enter a number to see it's factors." << '\n';
+    cout << "Next, please enter a number to see it's factors." << endl;
     cin >> factor_this;
     for (divided_by=1; (divided_by < up_to) && (divided_by < factor_this);)
         {
             if (factor_this%divided_by == 0)
             {
-                cout << factor_this << " divided by " << divided_by << " is " << (factor_this/divided_by) << '\n';
+                cout << factor_this << " divided by " << divided_by << " is " << (factor_this/divided_by) << endl;
             }
              ++divided_by;
         }
-    cout << "Would you like to calculate more? Y for yes, N for no." << '\n';
+    cout << "Would you like to calculate more? Y for yes, N for no." << endl;
     cin >> proceed;
     if (proceed=='Y' || proceed=='y')
     {
