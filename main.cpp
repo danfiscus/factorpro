46bbb27887bc78628375662dda7fd8f9335441a8
Jacob Bender <benderjacob44@gmail.com>
2014-09-21 15:34:11 -0400

    I made a number be able to divided by itself and refactored.
    Also, it now lists all factors of a number so it isn't bound anymore.

--- main.cpp
+++ main.cpp
@@ -3,23 +3,19 @@
 using namespace std;
 
 int factor_this;
-int divided_by, up_to;
+int divided_by;
 char proceed;
 
 int main()
 {
-    cout << "How many factors do you want listed?" << endl;
-    cin >> up_to;
-    up_to = up_to + 1;
-    cout << "Next, please enter a number to see it's factors." << endl;
+    cout << "Please enter a number to see it's factors." << endl;
     cin >> factor_this;
-    for (divided_by=1; (divided_by < up_to) && (divided_by < factor_this);)
+    for (divided_by=1;divided_by != factor_this+1;++divided_by)
         {
             if (factor_this%divided_by == 0)
             {
                 cout << factor_this << " divided by " << divided_by << " is " << (factor_this/divided_by) << endl;
             }
-             ++divided_by;
         }
     cout << "Would you like to calculate more? Y for yes, N for no." << endl;
     cin >> proceed;
