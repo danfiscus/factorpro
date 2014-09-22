6bb0eb6a5236518fea2fd851508f93d7798c87d7
Chris King <G.nius.ck@gmail.com>
2014-09-22 18:05:15 -0400

    Added comments, refactored a little, and used codeblocks AStyle reformatter

--- main.cpp
+++ main.cpp
@@ -2,9 +2,9 @@
 
 using namespace std;
 
-int factor_this;
-int divided_by, how_many;
-char proceed;
+int factor_this;    //The number to factor
+int how_many;       //How many factors the user wants listed (or -1 for all)
+char proceed;       //Whether or not to run the program again
 
 int main()
 {
@@ -13,22 +13,23 @@ int main()
 
     cout << "How many factors do you want listed?" << endl;
     cin >> how_many;
-    if (how_many == -1){
-	how_many == factor_this;
-	}
-    for (divided_by=1;(how_many!=0)&&(divided_by < factor_this);++divided_by)
+    if (how_many == -1)
+    {
+        how_many == factor_this; //Theorem: the number of factors a number has is less than itself
+    }
+    for (int divided_by=1; (how_many!=0)&&(divided_by < factor_this); ++divided_by)
+    {
+        if (factor_this%divided_by == 0)
         {
-            if (factor_this%divided_by == 0)
-            {
-                cout << factor_this << " divided by " << divided_by << " is " << (factor_this/divided_by) << endl;
-            	how_many-=1;
-	    }
+            cout << factor_this << " divided by " << divided_by << " is " << (factor_this/divided_by) << endl;
+            how_many-=1;
         }
+    }
     cout << "Would you like to calculate more? Y for yes, N for no." << endl;
     cin >> proceed;
     if (proceed == 'N' || proceed == 'n')
     {
-        cout << "I feel so, empty." <<endl;
+        cout << "I feel so, empty." <<endl; //Easter egg
         return 0;
     }
     main();
